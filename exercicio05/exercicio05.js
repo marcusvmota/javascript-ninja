180c4dbd182635425b6213eb8cb0906e59f600ad
marcusvmota <marcusmota08@gmail.com>
2022-07-28 12:33:50 -0300

    iniciando exercicio 05

---
+++ exercicio05/exercicio05.js
@@ -0,0 +1,82 @@
+/*
+Crie uma variável qualquer, que receba um array com alguns valores aleatórios
+- ao menos 5 - (fica por sua conta os valores do array).
+*/
+var myVar = [1,2,3,4,5];
+
+/*
+Crie uma função que receba um array como parâmetro, e retorne esse array.
+*/
+function myFunction(array){
+    return array;
+}
+/*
+Imprima o segundo índice do array retornado pela função criada acima.
+*/
+myFunction(array)[1]
+/*
+Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
+segundo, um número. A função deve retornar o valor de um índice do array que foi passado
+no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
+segundo parâmetro.
+*/
+function myFunction2([param1], param2){
+    return myFunction2.param1[1]
+}
+/*
+Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
+*/
+
+var myVar2 = ["vinicius", 2, true, "2", 2.5]
+
+/*
+Invoque a função criada acima, fazendo-a retornar todos os valores do último
+array criado.
+*/
+myFunction(myVar2);
+
+/*
+Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
+livro. Dentro dessa função, declare uma variável que recebe um objeto com as
+seguintes características:
+- esse objeto irá receber 3 propriedades, que serão nomes de livros;
+- cada uma dessas propriedades será um novo objeto, que terá outras 3
+propriedades:
+    - `quantidadePaginas` - Number (quantidade de páginas)
+    - `autor` - String
+    - `editora` - String
+- A função deve retornar o objeto referente ao livro passado por parâmetro.
+- Se o parâmetro não for passado, a função deve retornar o objeto com todos
+os livros.
+*/
+function book (nomeLivro){
+    var características = {
+        Livro1: { quantidadePaginas: 10, autor: "Vinicius", editora: "quadroa4"}
+    }
+}
+
+/*
+Usando a função criada acima, imprima o objeto com todos os livros.
+*/
+// ?
+
+/*
+Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
+usando a frase:
+"O livro [NOME_DO_LIVRO] tem [X] páginas!"
+*/
+// ?
+
+/*
+Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
+a frase:
+"O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
+*/
+// ?
+
+/*
+Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
+a frase:
+"O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
+*/
+// ?
